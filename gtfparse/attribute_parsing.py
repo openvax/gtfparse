afa721a166e89006255fd4aab5580ef82773dd4d
Moritz <mail@moritzs.de>
2017-11-03 15:36:04 +0100

    save all values instead of only the last one

--- gtfparse/attribute_parsing.py
+++ gtfparse/attribute_parsing.py
@@ -101,7 +101,10 @@ def expand_attribute_strings(
             value = intern(str(value))
             value_interned_strings[value] = value
 
-        column[i] = value
+        if column[i] == missing_value:
+            column[i] = value
+        else:
+            column[i] = "{},{}".format(column[i], value)
 
     logging.debug(
         "Memory usage after expanding GTF attributes: %0.4f MB" % (
