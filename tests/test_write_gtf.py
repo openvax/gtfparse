1976472c3d751d00dcaee56b3d0d21b0841f8870
Benoitdw <bw@oncodna.com>
2024-04-12 08:54:09 +0200

    Expand unit test

--- tests/test_write_gtf.py
+++ tests/test_write_gtf.py
@@ -6,6 +6,9 @@ REFSEQ_GTF_PATH = data_path("refseq.ucsc.small.gtf")
 
 
 def test_write_gtf(tmp_path):
-    gtf_dict = read_gtf(REFSEQ_GTF_PATH)
-    write_gtf(gtf_dict, tmp_path/"dummy_gtf.gtf")
-    assert  isinstance(read_gtf(str(tmp_path/"dummy_gtf.gtf")), DataFrame)
+    expected_gtf = read_gtf(REFSEQ_GTF_PATH)
+    write_gtf(expected_gtf, tmp_path/"dummy_gtf.gtf")
+    created_gtf = read_gtf(str(tmp_path/"dummy_gtf.gtf"))
+    assert  isinstance(created_gtf, DataFrame)
+    assert expected_gtf == created_gtf
+
